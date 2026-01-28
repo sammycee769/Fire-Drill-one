912e370db03cf01a49cd07f80da715d01fcf92e1
chris <samuelcristian769@gmail.com>
2026-01-28 07:52:38 +0100

    commit

--- TaskSeven.java
+++ TaskSeven.java
@@ -2,21 +2,23 @@ public class TaskSix{
     public static void main(String[] args){
 int countMultiple = 1;
 int countMultipletwo = 1;
+int countSum= 1;
     for(int count =1; count<=5; count++)
         for(int counter = 1;  counter<=10; counter++ )
  if (counter % 4 == 0 && counter ==4)
 {
 countMultiple *= counter;
-//System.out.print(countMultiple + " ");
+countSum += countMultiple;
+System.out.print(countSum + " ");
 }
 System.out.print(" ");
    for(int count =1; count<=5; count++)
         for(int counter = 1; counter<=10; counter++ )
 if (counter % 4 == 0 && counter == 8){
 countMultipletwo *= counter;
-//System.out.print(countMultipletwo + " ");
+System.out.print(countMultipletwo + " ");
 }
-int countSum = countMultipletwo + countMultiple;
+//int countSum = countMultipletwo + countMultiple;
 System.out.print(countSum);
 }
 }   
